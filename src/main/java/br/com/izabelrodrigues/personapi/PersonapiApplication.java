cbb845c699a39315913ad1a8f2a48f961945ca86
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2021-09-15 17:14:04 -0300

    - Adding Api documentation with OpenApi

--- src/main/java/br/com/izabelrodrigues/personapi/PersonapiApplication.java
+++ src/main/java/br/com/izabelrodrigues/personapi/PersonapiApplication.java
@@ -1,7 +1,13 @@
 package br.com.izabelrodrigues.personapi;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PersonapiApplication {
@@ -10,4 +16,11 @@ public class PersonapiApplication {
 		SpringApplication.run(PersonapiApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion, @Value("${springdoc.description}") String description) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Person API").version(appVersion).description(description)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
